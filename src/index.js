cc77172cb6f7e35c7a9ce144b6bfc689de51c7d6
Amnil Technologies <amnil@Amnils-MacBook-Pro.local>
2020-05-24 22:42:13 +0545

    initial Commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import AppCom from './AppCom';
+import AppC from './AppC';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AppC />
   </React.StrictMode>,
   document.getElementById('root')
 );
